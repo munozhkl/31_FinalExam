32a97c27918c5f1f1e981abc3aa60f410d4f23c7
munozhkl <munozhkl@rose-hulman.edu>
2018-05-24 13:20:57 -0400

    final 1a

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Kathi Munoz.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
 
 
@@ -90,9 +90,22 @@ def problem1a(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    sum = 0
+    for k in range(len(numbers)):
+        if k == 0:
+            sum = sum + numbers[k]
+            print(numbers[k])
+        if len(numbers) // 2 == k:
+            sum = sum + numbers[k]
+            print(numbers[k])
+        if (k+1) == len(numbers):
+            sum = sum + numbers[k]
+            print(numbers[k])
+    return sum
+
 
 
 def run_test_problem1b():
